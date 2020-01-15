d0381b8d14fb897f1a07504671e12080cbfdec6b
hermes k <hermes9563@hotmail.com>
2020-01-15 22:58:13 +0800

    TV Series List

---
+++ src/components/Loader/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import loaderSrc from '../../assets/loader.gif';
+const Loader = props => (
+  <div>
+    <img
+      style={{width: 50}}
+      alt="Loader icon"
+      src={loaderSrc} />
+  </div>
+)
+
+export default Loader
